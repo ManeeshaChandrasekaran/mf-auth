268fe89e575d300a07565c86a38df63edf11ec68
maneesha <maneesha@macminis-Mini.lan>
2025-09-11 16:01:57 +0530

    initial commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h2>Auth App: Login</h2>
+      <p>This is the login form.</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div>
+            <h2>Auth App: User Profile</h2>
+            <p>This is the User Profile</p>
+        </div>
+    );
+};
+
+export default UserProfile;
